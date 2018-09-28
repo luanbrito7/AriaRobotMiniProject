fa9c0b70ad354630d477dc506fe4cdabb4fa567a
Vitor Maia <vlma@cin.ufpe.br>
2018-09-27 23:03:12 -0300

    add get_robot_home util func

--- util.py
+++ util.py
@@ -12,3 +12,18 @@ def get_map_min_pos(filepath):
             return None
     read = read.split()
     return {x: float(read[1]), y: float(read[2])}
+
+def get_robot_home(filepath):
+    file = open(filepath)
+    read = file.readline()
+    while 'RobotHome' not in read:
+        read = file.readline()
+        if not read: # evita um loop inf se mapa nao tiver informacao de posicao min
+            return None
+    read = read.split()
+    try:
+        robot_home = {x: float(read[2]), y: float(read[3]), t: float(read[4])}
+        return robot_home
+    except Exception e:
+        print e
+    
