f5fc18e8a0a4685fc9fdfcf5e16543fc334ee30c
Vitor Maia <vlma@cin.ufpe.br>
2018-09-27 22:37:15 -0300

    add função de utilidade (pra caso o mapa varie)

---
+++ util.py
@@ -0,0 +1,14 @@
+"""
+retorna dicionario com coordenadas da posicao minima do mapa de caminho filepath ou None se nao achar 
+/usr/local/Aria/maps/office.map no osboxes
+"""
+def get_map_min_pos(filepath):
+    
+    file = open(filepath)
+    read = file.readline()
+    while 'LineMinPos' not in read:
+        read = file.readline()
+        if not read: # evita um loop inf se mapa nao tiver informacao de posicao min
+            return None
+    read = read.split()
+    return {x: float(read[1]), y: float(read[2])}
