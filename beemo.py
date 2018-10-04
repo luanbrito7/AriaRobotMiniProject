e3a01e2bd53255d33456e67c7bc1fcdbaafb9ad4
Vitor Maia <vlma@cin.ufpe.br>
2018-10-04 06:50:20 -0300

    tirando comentário gigante

--- beemo.py
+++ beemo.py
@@ -1,44 +1,3 @@
-"""
-#include "Aria.h"
-int main(int argc, char** argv)
-{
-  Aria::init();
-  ArArgumentParser parser(&argc, argv);
-  parser.loadDefaultArguments();
-  ArRobot robot;
-  ArRobotConnector robotConnector(&parser, &robot);
-  // Try connecting to the robot.
-  if(!robotConnector.connectRobot(&robot))
-  {
-    // Error!
-    ArLog::log(ArLog::Terse, "Error, could not connect to robot.\n");
-    robotConnector.logOptions();
-    Aria::exit(1);
-  }
-  // Run the ArRobot processing/task cycle thread.
-  robot.runAsync(true);
-  ArLaserConnector laserConnector(&parser, &robot, &robotConnector);
-  // Parse command line arguments (there may be arguments specifying 
-  // what lasers to try to connect to)
-  if(!Aria::parseArgs())
-  {
-    Aria::logOptions();
-    Aria::exit(2);
-  }
-  // Try connecting to all lasers specified in the robot's parameter file
-  // and in command line arguments
-  if(!laserConnector.connectLasers())
-  {
-    ArLog::log(ArLog::Terse, "Error, could not connect to lasers.\n");
-    Aria::logOptions();
-    Aria::exit(3);
-  }
-  // Now we're connected, and the robot and laser objects are running in 
-  // background threads reading and processing data. (You can get access
-  // to the ArLaser objects using ArRobot::findLaser() or
-  // ArRobot::getLaserMap()
-"""
-
 from AriaPy import *
 import sys
 import numpy
@@ -93,11 +52,14 @@ robot.enableMotors()
 pos_x = 1
 pos_y = 1
 while Aria.getRunning:
-    # robot.lock()
-    # poses = sonar.getCurrentBufferAsVector()
+    
+
+
+
+
     print robot.getPose()
     gotoPoseAction.setGoal(ArPose(pos_x, pos_y))
     pos_x *= 2
     pos_y *= 2
-    ArUtil.sleep(2000)
+    ArUtil.sleep(20000)
     # robot.unlock()
