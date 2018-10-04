620848be5148165c567c9c59a89d83e59fb45ffa
Vitor Maia <vlma@cin.ufpe.br>
2018-10-04 07:56:51 -0300

    ops

--- beemo.py
+++ beemo.py
@@ -69,7 +69,7 @@ while Aria.getRunning:
         
         log = "globalCoords = ({}, {})    mapCoords = ({}, {})".format(globalCoords['x'], globalCoords['y'], mapCoords[0], mapCoords[1])
         print log
-        # mapa.itemset(mapCoords, 1)
+        mapa.itemset(mapCoords, 1)
         # print mapa
 
 
